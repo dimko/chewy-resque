925dc3ae340e69081f46cb89f4e855fb5a445242
Dimko <deemox@gmail.com>
2015-03-24 19:15:54 -0700

    fixed chewy's type patching

--- lib/chewy/resque.rb
+++ lib/chewy/resque.rb
@@ -6,9 +6,14 @@ require_relative "strategy/background_atomic"
 require_relative "strategy/background_urgent"
 require_relative "resque/version"
 require_relative "resque/worker"
-require_relative "type"
 
 module Chewy
   module Resque
+  end
+
+  class Type
+    def self.derived_name
+      name.underscore.sub!(/_index\/(#{type_name})\z/, '#\\1')
+    end
   end
 end
--- lib/chewy/type.rb
+++
@@ -1,7 +0,0 @@
-module Chewy
-  class Type
-    def self.derived_name
-      name.underscore.sub!(/_index\/(#{type_name})\z/, '#\\1')
-    end
-  end
-end
