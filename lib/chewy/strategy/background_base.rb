500f8e9318d2980c1cbce63c5464a6fb69f364a5
Dimko <deemox@gmail.com>
2015-03-25 14:46:24 -0700

    enqueue job only if ids are present

--- lib/chewy/strategy/background_base.rb
+++ lib/chewy/strategy/background_base.rb
@@ -2,7 +2,9 @@ module Chewy
   class Strategy
     class BackgroundBase < Base
       def enqueue(type, ids)
-        ::Resque.enqueue Chewy::Resque::Worker, type.derived_name, ids
+        unless ids.empty?
+          ::Resque.enqueue Chewy::Resque::Worker, type.derived_name, ids
+        end
       end
     end
   end
